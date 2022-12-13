c8dd02b0a0741f1407c2677bd7c1337cde9c593b
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2022-12-13 16:11:16 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -15,3 +15,22 @@ for var in $@; do
 		echo 'noooonnn'
 	fi
 done
+
+## tri 
+
+recur=0
+echo $#
+echo $@
+for var in $@; do
+	if [ $var == "-a" ] ; then
+		echo 'argument 0'
+		recur=$((recur+1))
+		sort -t";" -k2 meteo_filtered_data_v1.csv | cut 
+	else 
+		echo 'noooonnn'
+	fi
+	if [ $recur -gt 1 ] ; then
+		echo $recur
+		return 1
+	fi
+done
