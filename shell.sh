925b67a5454d1683f3521dbe71fc01a98d3ef023
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2022-12-13 15:20:04 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-for i in $(seq 1 $#) ; do
-	case ${!i} in
-	  
+##   ${!i}  permt de réutiliser la valeur de i comme nom de variable
+
+
+##   petit programme pour regarder argument
+
+
+echo $#
+echo $@
+for var in $@; do
+	if [ $var == "-a" ] ; then
+		echo 'argument 0'
+	else 
+		echo 'noooonnn'
+	fi
 done
