3774fa63f8a471001948798af1015479c0aeb825
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2023-02-01 21:40:31 +0100

    Update programme.sh

--- programme.sh
+++ programme.sh
@@ -1,62 +1,78 @@
 #!/bin/bash
 
-##   ${!i}  permt de réutiliser la valeur de i comme nom de variable
-
+## variables for recurrence
 t1=0
 t2=0
+t3=0
 p1=0
+p2=0
+p3=0
 h=0
 m=0
+w=0
 
+## taking the number and the list of arguments in other variables because the while / shift erase them
 i=$#
 arg_list=$@
+
+## variable to stock the file name
 file=not_define
 
+##verifying if the file is precised and if so replace the name in file
 while [ $# -gt 0 ] ; do
 	case "$1" in
-	-f) file=$2 ;;
-	--help) cat readme.txt
+	-f) file=$2 ;; ## the file name is supposed to be after -f
+	--help) cat readme.txt ## printing the readme for --help
+	exit 1 ;;
 	esac 
 shift 
 done
 
-
+## verifying the existence of the file
 if [ ! -e $file ]
 then
-    echo "le fichier n'existe pas"
+    echo "the file does not exist"
     exit 1
 fi
 
+make ## makefile
+if [ $? -ne 0 ] ; then ## verifying if it worked
+	echo "makefile problem detected, please make sure all the .c files are here"
+	exit 1
+fi
+
+## to erase
 echo $i
 echo $arg_list
 
-
+## recurrence for the localisation
 recurrence=0
 
+##default sorting type 
 sort=abr
 
+## no done by c language
 reverse=
 
-## localisation
-
+## function of return used to see if the command line was executed and if not exit the program
 fct_return()
 {
 	echo "error occured when executing -" $1
 	exit 1	
 }
 
-
+## localisation
 fct_G()
 {
-	if [ $recurrence -gt 0 ]
+	if [ $recurrence -gt 0 ] ## not possible to have more than one localistion precised
 	then 
 		echo "you cannot select more than one location, please try again"
 	else
-		cat $file | tr  ',' ';' | awk -F ';' '{if($10>'"1.5"' && $10<'"6.5"' && $11<'"-50"' && $11>'"-55"'){print $0}}' > localisation.txt 
+		cat $file | tr  ',' ';' | awk -F ';' '{if($10>'"1.5"' && $10<'"6.5"' && $11<'"-50"' && $11>'"-55"'){print $0}}' > localisation.txt ## using awk to take only the localisation wanted with the coordinates
 		if [ $? -ne 0 ] ; then
 			fct_return 'G'
 		fi
-		recurrence=$((recurrence+1))
+		recurrence=$((recurrence+1)) 
 	fi
 }
 
@@ -130,6 +146,7 @@ fct_Q()
 	fi
 }
 
+## looking in the list of arguments if localisation was precised, if not, all the file is transfered to localisation.txt directly
 for var in $arg_list; do
 	case $var in
 		"-G") fct_G ;;
@@ -156,14 +173,16 @@ done
 
 fct_t1()
 {
-	if [ $t1 -gt 0 ]
+	if [ $t1 -gt 0 ] ## same recurrence than for localisation needed
 	then 
 		echo $t1
 		echo "problem detected too many arguments of the same type"
 	else
-		cut -d";" -f1,13,14 localisation.txt > t1.txt
+		cut -d";" -f1,12,13,14 localisation.txt > t1.txt ##taking only the column we will be using
 		t1=$((t1+1))
-		echo $reverse
+		./t1 t1.txt output_t1.txt --$sort ## executing the c program and looking if it succeed to finish without any errors
+		
+		gnuplot -persist gnp_1.gnp ## writing the gnuplot scheme associated
 		
 	fi
 }
@@ -176,7 +195,10 @@ fct_t2()
 		echo "problem detected too many arguments of the same type"
 	else
 		cut -d";" -f1,2,13,14 localisation.txt | tr -d '-' > t2.txt
-		t3=$((t3+1))
+		t2=$((t2+1))
+		./t2 t2.txt output_t2.txt --$sort
+		
+		gnuplot -persist gnp_2.gnp
 	fi
 }
 
@@ -189,6 +211,9 @@ fct_t3()
 	else
 		cut -d";" -f1,2,13,14 localisation.txt > t3.txt
 		t3=$((t3+1))
+		./t3 t3.txt output_t3.txt --$sort
+		
+		gnuplot -persist gnp_3.gnp
 	fi
 }
 
@@ -199,8 +224,26 @@ fct_p1()
 		echo $p1
 		echo "problem detected too many arguments of the same type"
 	else
-		cut -d";" -f1,7 localisation.txt > pression.txt
+		cut -d";" -f1,7 localisation.txt > p1.txt
 		p1=$((p1+1))
+		./p1 p1.txt output_p1.txt --$sort
+		
+		gnuplot -persist gnp_1.gnp
+	fi
+}
+
+fct_p2()
+{
+	if [ $p2 -gt 0 ]
+	then 
+		echo $p2
+		echo "problem detected too many arguments of the same type"
+	else
+		cut -d";" -f1,2,7 localisation.txt > p2.txt
+		p2=$((p2+1))
+		./p2 p2.txt output_p2.txt --$sort
+		
+		gnuplot -persist gnp_2.gnp
 	fi
 }
 
@@ -213,6 +256,9 @@ fct_h()
 	else
 		cut -d";" -f1,15 localisation.txt > altitude.txt
 		h=$((h+1))
+		./h h.txt output_h.txt --$sort
+		
+		gnuplot -persist gnp_h.gnp
 	fi
 }
 
@@ -225,18 +271,24 @@ fct_m()
 	else
 		cut -d";" -f1,6 localisation.txt > humidite.txt
 		m=$((m+1))
+		./m m.txt output_m.txt --$sort
+		
+		gnuplot -persist gnp_m.gnp
 	fi
 }
 
 fct_w()
 {
-	if [ $m -gt 0 ]
+	if [ $w -gt 0 ]
 	then 
-		echo $m
+		echo $w
 		echo "problem detected too many arguments of the same type"
 	else
 		cut -d";" -f1,4,5 localisation.txt > vent.txt
-		m=$((m+1))
+		w=$((w+1))
+		./w w.txt output_w.txt --$sort
+		
+		gnuplot -persist gnp_w.gnp
 	fi
 }
 
@@ -257,7 +309,7 @@ done
 
 
 
-
+## arguments
 for var in $arg_list; do
 	case $var in
 		"-t1") fct_t1 ;;
@@ -271,8 +323,9 @@ for var in $arg_list; do
 	esac 
 done
 
+## in case not any mods selected, exit
 if [ $recurrence -eq 0 ] ; then
 	echo "please select an option"
 	exit 1
 fi
-echo $sort
+
